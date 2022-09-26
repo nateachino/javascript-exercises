5c9c40b81f9f7ec894388aa675fce2b828b3b993
nateachino <natealleyne88@gmail.com>
2022-09-26 13:25:27 -0400

    Finished 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    let wrd = ''
+    for (i=0;i<words.length;i++){
+        wrd += words[words.length-1-i];
+    }
+    return wrd
 };
 
 // Do not edit below this line
