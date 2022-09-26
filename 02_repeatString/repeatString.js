84a163d43e97f85b2954534631d817e0c8a2df91
nateachino <natealleyne88@gmail.com>
2022-09-26 11:52:59 -0400

    Finished 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    if (num<0){
+        return "ERROR";
+    }else{
+    let word = ''
+    for (i=0;i<num;i++){
+        word+=string;
+    }
+    return word;
+}
 };
 
 // Do not edit below this line
