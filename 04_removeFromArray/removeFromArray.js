123afed45f3e5c687ea063af6f89995749c13ae4
nateachino <natealleyne88@gmail.com>
2022-09-27 09:22:20 -0400

    Changes made to #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (...list) {
+  let newArray = list.splice(list);
+  let splicedArray = newArray.splice(list);
 
+  return newArray;
 };
 
 // Do not edit below this line
